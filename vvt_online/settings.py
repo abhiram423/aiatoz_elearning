e068283d64d42f2ef9935e3a11d6be6df448994e
abhiram423 <abhiram170803@gmail.com>
2026-05-27 15:12:24 +0530

    render database connected

--- vvt_online/settings.py
+++ vvt_online/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY_AI')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*', 'aiatoz.org', 'www.aiatoz.org']
 
 
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'vvt_online.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'AIATOZ',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('DB_PW_AI'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 

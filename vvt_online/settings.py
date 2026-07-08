c73f3fbc73dd3ac91a24689133d14d960246416a
abhiram423 <abhiram170803@gmail.com>
2026-07-08 12:46:30 +0530

    Disabled debug mode after database schema synchronization

--- vvt_online/settings.py
+++ vvt_online/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY_AI')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*', 'aiatoz.org', 'www.aiatoz.org']
 
 
